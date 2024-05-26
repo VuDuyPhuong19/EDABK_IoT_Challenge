d29a559557cf121a6be0fb1362bd822f2430a869
VuDuyPhuong19 <146603946+VuDuyPhuong19@users.noreply.github.com>
2024-05-26 12:50:54 +0700

    update

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ require('dotenv').config({path: path.resolve(__dirname, './config.env')});
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    // const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect('mongodb+srv://vuduyphuong:Phuong153280@vuduyphuong.odzmo8u.mongodb.net/sensor?retryWrites=true&w=majority&appName=vuduyphuong');
     console.log(`MongoDB connected: ${conn.connection.host}`.green.bold);
   } catch (err) {
     console.error(`Error: ${err.message}`.red.bold);
